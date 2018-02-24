111f796873aa57747358265715ce7794ffce09b8
Ian Ewing <ian.ewing@gmail.com>
2018-02-23 18:50:11 -0700

    Latest attempt at a consumer test

--- src/test/java/com/calculator/KafkaConsumerTest.java
+++ src/test/java/com/calculator/KafkaConsumerTest.java
@@ -15,9 +15,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.listener.config.ContainerProperties;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
@@ -37,4 +42,50 @@ import static org.junit.Assert.assertThat;
 @SpringBootTest
 @DirtiesContext
 public class KafkaConsumerTest {
+    private Integer i1 = 0;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerTest.class);
+
+    private static String RECEIVER_TOPIC = "input";
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    private KafkaTemplate<String, Integer> template;
+
+    @Autowired
+    private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+    @ClassRule
+    public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, RECEIVER_TOPIC);
+
+    @Before
+    public void setUp() throws Exception {
+        // set up the Kafka producer properties
+        Map<String, Object> senderProperties =
+                KafkaTestUtils.senderProps(embeddedKafka.getBrokersAsString());
+
+        // create a Kafka producer factory
+        ProducerFactory<String, Integer> producerFactory =
+                new DefaultKafkaProducerFactory<>(senderProperties);
+
+        // create a Kafka template
+        template = new KafkaTemplate<>(producerFactory);
+        // set the default topic to send to
+        template.setDefaultTopic(RECEIVER_TOPIC);
+
+        // wait until the partitions are assigned
+        for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry
+                .getListenerContainers()) {
+            System.out.println("NOTICE ME " + kafkaListenerEndpointRegistry.getListenerContainers().toString());
+            ContainerTestUtils.waitForAssignment(messageListenerContainer,
+                    embeddedKafka.getPartitionsPerTopic());
+        }
+    }
+
+    @Test
+    public void testReceive() throws Exception {
+        // send the message
+        template.sendDefault(i1);
+
+    }
 }
